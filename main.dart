e42de724d0fcaa5bc2ced3efafeae04c73a034d9
vrinda kohli <vrinda.18bcs1012@abes.ac.in>
2020-10-17 15:30:40 +0530

    final main

--- main.dart
+++ main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app_hackathon/screens/home_screen.dart';
 import 'package:flutter_app_hackathon/screens/login_screen.dart';
 import 'package:flutter_app_hackathon/screens/signup_screen.dart';
+import 'package:flutter_app_hackathon/screens/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,9 +10,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Login App",
-      theme: ThemeData(primaryColor: Colors.blue),
-      home: LoginScreen(),
+      //title: "Login App",
+      theme: ThemeData(primaryColor: Colors.white, fontFamily: "nunito"),
+      debugShowCheckedModeBanner: false,
+
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          child: loginpage(),
+        ),
+      ),
       routes: {
         SignupScreen.routeName: (ctx) => SignupScreen(),
         LoginScreen.routeName: (ctx) => LoginScreen(),
